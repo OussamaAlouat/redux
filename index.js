2da33996e780542d6507393353a76608d497ad39
Oussama Alouat <al317257@uji.es>
2021-09-24 12:14:11 +0200

    Add reducers

--- index.js
+++ index.js
@@ -28,3 +28,33 @@ const createClaim = (name, amountOfMoneyToCollect) => {
     }
   }
 }
+
+// Reducers (departments)
+const claimsHistory = (oldListOfClaims = [], action) => {
+  if (action.type === 'CREATE_CLAIM') {
+    return [ ...oldListOfClaims, payload ]
+  }
+
+  return oldListOfClaims;
+};
+
+
+const acounting = (bagofmoney, action) => {
+  if (action.type === 'CREATE_CLAIM') {
+    return bagofmoney - action.payload.amountOfMoneyToCollect
+  } else if (action.type === 'CREATE_POLICY') {
+    return bagofmoney + action.payload.amount;
+  }
+
+  return bagofmoney;
+};
+
+const policies = (listofPolicies = [],  action) => {
+  if (action.type === 'CREATE_POLICY') {
+    return [ ...listofPolicies, action.payload.name ];
+  } else if( action.type === 'DELETE_POLICY') {
+    return listofPolicies.filter((policy) => policy !== action.payload.name);
+  }
+
+  return listofPolicies;
+}
